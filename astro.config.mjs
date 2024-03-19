fb2ad26b5cd16417855bde287a8b2e5afb409654
John Farrell <jsfarrell12@gmail.com>
2024-03-18 22:43:24 -0400

    init

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import mdx from "@astrojs/mdx";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
+// Takes too long to build, especially with image library
+import compress from "astro-compress";
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://jsfarrell.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    compress({
+      css: true,
+      html: true,
+      js: true,
+      img: false,
+      svg: false,
+    }),
+    prefetch(),
+  ],
+});
---
+++ src/consts.ts
@@ -0,0 +1,15 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "John Farrell";
+export const SITE_DESCRIPTION = "Computer science student at Brown University";
+export const SITE_AUTHOR = "John Farrell";
+export const SITE_EMAIL = "john_farrell@brown.edu";
+export const GITHUB_REPO = "johnsfarrell/site";
+export const SITE_URL = "https://jsfarrell.com";
+
+export const SOCIAL_MEDIA = {
+  github: "https://github.com/johnsfarrell/",
+  linkedin: "https://www.linkedin.com/in/johnsfarrell/",
+  instagram: "https://www.instagram.com/jahn_farrell/",
+};
