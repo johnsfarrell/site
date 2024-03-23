395cad6bfb5006251d87d26ef7728b1d35b3b9a2
John Farrell <jsfarrell12@gmail.com>
2024-03-23 16:43:26 -0400

    latex

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
-// Takes too long to build, especially with image library
 import compress from "astro-compress";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+
 export default defineConfig({
   site: "https://johnfarrell.io",
   integrations: [
@@ -20,4 +21,8 @@ export default defineConfig({
     }),
     prefetch(),
   ],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
